aeac31b4bb16560ee6358e0c916e9f831bbbf842
Pedro Lucas Xavier <pedrolucas.xs@gmail.com>
2022-09-22 15:26:02 -0300

    entrega realizada parcialmente com as orientações do canvas

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from utils import json_handler
+
 if __name__ == "__main__":
-    # Utilize essa área para testes com print
+    # print(json_handler.read_json('./menu.json'))
+    item = {'name': 'CHURROS DO M5', 'price': 5.0, 'id': 35}
+    print(json_handler.write_json('./menu.json', item))
     ...
---
+++ utils/json_handler.py
@@ -0,0 +1,60 @@
+import json
+
+
+def read_json(path: str):
+    with open(path, 'r', encoding='utf8') as file:
+        items = json.load(file)
+    if len(items) != 0:
+        print(len(items))
+        return items
+    return []
+
+
+def write_json(path: str, item: dict):
+    items_list = read_json(path)
+    with open(path, 'w', encoding='utf8') as file:
+        if len(items_list) == 0:
+            new_item = {
+                'id': 1,
+                'name': item['name'],
+                'price': item['price'],
+            }
+            items_list.append(new_item)
+            json.dump(items_list, file, indent=2)
+        else:
+            found = [
+                items_in_list for items_in_list in items_list 
+                if items_in_list['name'] == item['name']
+            ]
+            if len(found) != 0:
+                answer = input('We already had an item with this name, want to proceed instead?(write Y or N)')
+                if answer == 'Y':
+                    ids = [
+                        item_in_list['id'] for item_in_list in items_list
+                    ]
+                    generated_id = max(tuple(ids))
+                    new_item = {
+                        'id': generated_id + 1,
+                        'name': item['name'],
+                        'price': item['price'],
+                    }
+                    items_list.append(new_item)
+                    json.dump(items_list, file, indent=2)
+                    return new_item
+                else:
+                    json.dump(items_list, file, indent=2)
+                    return 'Try register other item'
+            else:
+                ids = [
+                    item_in_list['id'] for item_in_list in items_list
+                    ]
+                generated_id = max(tuple(ids))
+                new_item = {
+                'id': generated_id + 1,
+                'name': item['name'],
+                'price': item['price'],
+            }
+                items_list.append(new_item)
+                json.dump(items_list, file, indent=2)
+                return new_item
+                    
